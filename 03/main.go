a4dceaee2d50ce8793aa6af4271470b8362ef53f
Brian Derr <brian@derrclan.com>
2024-12-23 10:35:42 -0800

    [D03] second star earned

--- 03/main.go
+++ 03/main.go
@@ -29,21 +29,29 @@ func readInput() []mulPair {
 	}
 	defer inFile.Close()
 
-	rgx := regexp.MustCompile(`mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)`)
+	rgx := regexp.MustCompile(`do(?:n't)?\(\)|mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)`)
 	var pairs []mulPair
 	sc := bufio.NewScanner(inFile)
+	var do bool = true
 	for sc.Scan() {
 		for _, match := range rgx.FindAllStringSubmatch(sc.Text(), -1) {
-			if len(match) == 3 {
-				a, err := strconv.Atoi(match[1])
-				if err != nil {
-					panic(err)
+			switch match[0] {
+			case "do()":
+				do = true
+			case "don't()":
+				do = false
+			default:
+				if do && len(match) == 3 {
+					a, err := strconv.Atoi(match[1])
+					if err != nil {
+						panic(err)
+					}
+					b, err := strconv.Atoi(match[2])
+					if err != nil {
+						panic(err)
+					}
+					pairs = append(pairs, mulPair{a, b})
 				}
-				b, err := strconv.Atoi(match[2])
-				if err != nil {
-					panic(err)
-				}
-				pairs = append(pairs, mulPair{a, b})
 			}
 		}
 	}
